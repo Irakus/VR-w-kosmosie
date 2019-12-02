7f10f3d8162c417923837bda63c79b62d4f17667
Adam <a-zieliski@wp.pl>
2019-12-02 20:43:02 +0100

    Add damage system

---
+++ Assets/ShipDamage.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipDamage : MonoBehaviour
+{
+    [SerializeField] private int health;
+    [SerializeField] private AudioClip[] oneDamageClips;
+    private AudioSource _audioSource;
+    
+    
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ReceiveDamage(int damage, Vector3 hitPosition)
+    {
+        health -= damage;
+        if (_audioSource != null)
+        {
+            transform.position = hitPosition;
+            _audioSource.PlayOneShot(oneDamageClips[Random.Range(1,oneDamageClips.Length)]);
+        }
+    }
+}
--- Assets/_VRSpaceships/Scripts/Bullet.cs
+++ Assets/_VRSpaceships/Scripts/Bullet.cs
@@ -1,12 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float velocity;
+    
+    private int _damage = 1;
     private float _lifetime = 10.0f;
-
+    private bool _hasHit = false;
+    [SerializeField] private GameObject hitEffectPrefab;
 
     // Update is called once per frame
     void Update()
@@ -14,7 +20,20 @@ public class Bullet : MonoBehaviour
         var time = Time.deltaTime;
         transform.position += time * velocity * transform.up;
         _lifetime -= time;
-        if (_lifetime<0)
+        if (_lifetime < 0)
             Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (_hasHit)
+            return;
+        
+        _hasHit = true;
+        var ship = other.attachedRigidbody;
+        var shipDamage = ship.GetComponentInChildren<ShipDamage>();
+        shipDamage.ReceiveDamage(this._damage, transform.position);
+        Instantiate(hitEffectPrefab, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 }
