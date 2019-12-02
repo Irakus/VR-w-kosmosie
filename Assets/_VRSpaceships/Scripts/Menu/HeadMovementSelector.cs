7b1a7d13b7a9c8165c5eba978d316e7b3c221822
Irakus <patrak122@gmail.com>
2019-12-02 21:21:09 +0100

    Added need for button press

--- Assets/_VRSpaceships/Scripts/Menu/HeadMovementSelector.cs
+++ Assets/_VRSpaceships/Scripts/Menu/HeadMovementSelector.cs
@@ -17,9 +17,7 @@ public class HeadMovementSelector : MonoBehaviour
 
     void Start()
     {
-        //Fetch the Raycaster from the GameObject (the Canvas)
         _raycaster = GetComponent<GraphicRaycaster>();
-        //Fetch the Event System from the Scene
         _eventSystem = GetComponent<EventSystem>();
         _camera = Camera.main;
     }
@@ -27,34 +25,27 @@ public class HeadMovementSelector : MonoBehaviour
 
     void Update()
     {
-        //Check if the left Mouse button is clicked
-        if (true)
+        _pointerEventData = new PointerEventData(_eventSystem);
+        _pointerEventData.position = _camera.WorldToScreenPoint(_fillBar.transform.position);
+        List<RaycastResult> results = new List<RaycastResult>();
+        _raycaster.Raycast(_pointerEventData, results);
+
+        if (results.Count == 0)
         {
-            //Set up the new Pointer Event
-            _pointerEventData = new PointerEventData(_eventSystem);
-            //Set the Pointer Event Position to that of the mouse position
-            _pointerEventData.position = _camera.WorldToScreenPoint(_fillBar.transform.position);
-
-            //Create a list of Raycast Results
-            List<RaycastResult> results = new List<RaycastResult>();
-
-            //Raycast using the Graphics Raycaster and mouse click position
-            _raycaster.Raycast(_pointerEventData, results);
-            Debug.DrawRay(_camera.transform.position, _camera.transform.position + _camera.transform.forward*50.0f,new Color(1.0f,0.0f,0.0f));
-            Debug.Log(results[0].gameObject);
-            //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
-            if (results.Count == 0 || results[0].gameObject != _currentlySelected)
+            _currentlySelected = null;
+            _fillBar.fillAmount = 0.0f;
+        }
+        else if (results[0].gameObject != _currentlySelected || !Input.GetButton("MenuButtonAccept"))
+        {
+            _currentlySelected = results[0].gameObject;
+            _fillBar.fillAmount = 0.0f;
+        }
+        else
+        {
+            _fillBar.fillAmount += 0.5f * Time.deltaTime;
+            if (_fillBar.fillAmount >= 1.0f)
             {
-                _currentlySelected = results[0].gameObject;
-                _fillBar.fillAmount = 0.0f;
-            }
-            else
-            {
-                _fillBar.fillAmount += 0.2f * Time.deltaTime;
-                if (_fillBar.fillAmount >= 1.0f)
-                {
-                    _currentlySelected.GetComponent<Button>().onClick.Invoke();
-                }
+                _currentlySelected.GetComponent<Button>().onClick.Invoke();
             }
         }
     }
