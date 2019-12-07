ca8383944f26456e654faef320b6c1c1630a4634
Irakus <patrak122@gmail.com>
2019-12-07 20:50:50 +0100

    multi display

--- Assets/_VRSpaceships/Scripts/Misc/ScreenChecker.cs
+++ Assets/_VRSpaceships/Scripts/Misc/ScreenChecker.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR;
+
 
 public class ScreenChecker : MonoBehaviour
 {
+    [SerializeField]
+    private Camera TPPCamera;
+    [SerializeField]
+    private Camera SideCamera;
     void Start()
     {
 
         if (Display.displays.Length >= 2)
         {
             Display.displays[1].Activate();
+        }
+
+        if (!Application.isEditor && !Debug.isDebugBuild && XRDevice.isPresent)
+        {
+            
+            TPPCamera.targetDisplay = 0;
+            SideCamera.targetDisplay = 1;
         }
     }
 
