8002056391129d057551ab83b7dbdc1c5f94afc4
Irakus <patrak122@gmail.com>
2020-01-15 22:31:12 +0100

    radar battle fix

--- Assets/RaceRadar.cs
+++ Assets/RaceRadar.cs
@@ -7,22 +7,27 @@ public class RaceRadar : MonoBehaviour
     RaceManager _raceManager;
     [SerializeField]
     GameObject arrow;
+    bool race = false;
     void Start()
     {
         _raceManager = FindObjectOfType<RaceManager>();
+        if (_raceManager != null) race = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Transform ringPos = _raceManager.GetCurrentRingPosition();
-        if(ringPos == null)
+        if (race)
         {
+            Transform ringPos = _raceManager.GetCurrentRingPosition();
+            if (ringPos == null)
+            {
 
-        }
-        else
-        {
-            arrow.transform.LookAt(ringPos);
+            }
+            else
+            {
+                arrow.transform.LookAt(ringPos);
+            }
         }
     }
 }
